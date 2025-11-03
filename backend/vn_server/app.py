25c17cb9005dba4d881b7281737bd78157da2c41
Lara Ersoy <laraersoy@gmail.com>
2025-11-04 00:49:12 +0100

    Cleaned repo: ignore venv and cache files

--- backend/vn_server/app.py
+++ backend/vn_server/app.py
@@ -7,8 +7,20 @@ def create_app():
 
     @app.route("/")
     def home():
-        return jsonify({"message": "Eureka!"})
+        return jsonify({"message": "Visual novel!"})
     
+    # First scene with background included
+    @app.route("/api/scene")
+    def get_scene():
+        scene_data = {
+            "title": "Once there was a powerful mercenary. For coin, she would use her sword in any situation.",
+            "background": "Princess.PNG",
+            "dialogue": [
+                {"speaker": "Mercenary", "line": "Recently, a got a very interesting request from the Kingdom of Hearts."},
+                {}
+            }
+        }
+        
     return app
 
 if __name__ == "__main__":
